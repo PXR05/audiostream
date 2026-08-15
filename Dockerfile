3117aa16a64cb147d18280f617047e81bd371d0a
PXR <pxr2709@gmail.com>
2026-08-16 00:28:19 +0700

    po token

--- Dockerfile
+++ Dockerfile
@@ -35,7 +35,7 @@ RUN curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o 
 RUN chmod a+rx /usr/bin/yt-dlp
 
 # Uncomment if using the bgutil-ytdlp-pot-provider plugin with yt-dlp:
-# RUN apt-get install -y python3-pip && pip install --break-system-packages -U bgutil-ytdlp-pot-provider
+RUN apt-get install -y python3-pip && pip install --break-system-packages -U bgutil-ytdlp-pot-provider
 
 COPY --from=build /app/server.js ./server.js
 COPY --from=build /app/src/db/migrations ./src/db/migrations
--- src/modules/audio/providers/youtube.ts
+++ src/modules/audio/providers/youtube.ts
@@ -110,13 +110,13 @@ export class YoutubeProvider extends BaseAudioProvider {
     return [
       ...(cookies ? ["--cookies", "cookies.txt"] : []),
       "--extractor-args",
-      "youtube:player_client=android,ios",
+      // "youtube:player_client=android,ios",
 
       // POT: Uncomment the lines below and comment out "youtube:player_client=android,ios" above
       // if you run the BgUtils POT provider container and want full web/mweb format access:
-      // ...(potUrl ? ["--extractor-args", `youtubepot-bgutilhttp:base_url=${potUrl}`] : []),
-      // "--extractor-args",
-      // "youtube:player_client=default,mweb",
+      ...(potUrl ? ["--extractor-args", `youtubepot-bgutilhttp:base_url=${potUrl}`] : []),
+      "--extractor-args",
+      "youtube:player_client=default,mweb",
 
       "-f",
       "bestaudio",
